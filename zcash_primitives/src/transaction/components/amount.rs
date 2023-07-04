10aa57d1cb1236406203e308fc1fd957bbd15499
Dmitry Demin <dmitry@qed-it.com>
2023-07-04 11:29:32 +0200

    Modify arb_amount function to generate amount values from tests from range 1..MAX_MONEY instead of -MAX_MONEY..MAX_MONEY, i.e. not generate negative or zero amount values

--- zcash_primitives/src/transaction/components/amount.rs
+++ zcash_primitives/src/transaction/components/amount.rs
@@ -299,7 +299,7 @@ pub mod testing {
     use super::{Amount, MAX_MONEY};
 
     prop_compose! {
-        pub fn arb_amount()(amt in -MAX_MONEY..MAX_MONEY) -> Amount {
+        pub fn arb_amount()(amt in 1..MAX_MONEY) -> Amount {
             Amount::from_i64(amt).unwrap()
         }
     }
