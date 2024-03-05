2c085b11f142af7641b126361b52be7486fa45cd
Dmitry Demin <dmitry@qed-it.com>
2024-03-05 15:14:33 +0100

    Minor fix or Rust code format (because of cargo fmt error)

--- zcash_primitives/src/transaction/components.rs
+++ zcash_primitives/src/transaction/components.rs
@@ -1,12 +1,12 @@
 //! Structs representing the components within Zcash transactions.
 
 pub mod amount;
+pub mod note;
 pub mod orchard;
 pub mod sapling;
 pub mod sprout;
 pub mod transparent;
 pub mod tze;
-pub mod note;
 
 pub use self::{
     amount::Amount,
