2ba4854c73d16d82c5f6a90e981a172505eb9fc8
alexeykoren <2365507+alexeykoren@users.noreply.github.com>
2024-12-16 13:53:47 +0100

    Fmt

--- zcash_primitives/src/transaction/components/orchard.rs
+++ zcash_primitives/src/transaction/components/orchard.rs
@@ -8,6 +8,7 @@ use crate::transaction::components::issuance::read_asset;
 use crate::transaction::{OrchardBundle, Transaction};
 use byteorder::ReadBytesExt;
 use nonempty::NonEmpty;
+use orchard::domain::OrchardDomainCommon;
 use orchard::{
     bundle::{Authorization, Authorized, Flags},
     domain::OrchardDomainCommon,
