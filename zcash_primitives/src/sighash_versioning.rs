a1efe9735f0e744d4e4e367b72540ac0374ab135
Constance Beguier <constance@qed-it.com>
2025-09-25 09:43:10 +0200

    Movee ISSUE_SIGHASH_VERSION_TO_BYTES under nu7 flag

--- zcash_primitives/src/sighash_versioning.rs
+++ zcash_primitives/src/sighash_versioning.rs
@@ -5,10 +5,10 @@
 use alloc::{collections::BTreeMap, vec::Vec};
 use lazy_static::lazy_static;
 
-use orchard::{
-    issuance_sighash_versioning::IssueSighashVersion,
-    orchard_sighash_versioning::OrchardSighashVersion,
-};
+use orchard::orchard_sighash_versioning::OrchardSighashVersion;
+
+#[cfg(zcash_unstable = "nu7")]
+use orchard::issuance_sighash_versioning::IssueSighashVersion;
 
 lazy_static! {
     pub(crate) static ref ORCHARD_SIGHASH_VERSION_TO_BYTES: BTreeMap<OrchardSighashVersion, Vec<u8>> =
@@ -23,6 +23,7 @@ pub(crate) fn to_orchard_version(bytes: Vec<u8>) -> Option<OrchardSighashVersion
         .map(|(k, _)| k.clone())
 }
 
+#[cfg(zcash_unstable = "nu7")]
 lazy_static! {
     pub(crate) static ref ISSUE_SIGHASH_VERSION_TO_BYTES: BTreeMap<IssueSighashVersion, Vec<u8>> =
         BTreeMap::from([(IssueSighashVersion::V0, vec![0],)]);
