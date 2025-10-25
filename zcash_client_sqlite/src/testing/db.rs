ef7d96f4ad5422842393ff84e59321653b6e2ae8
Jack Grigg <jack@electriccoin.co>
2025-10-25 07:45:11 +0000

    Migrate to Rust 2024 edition

--- zcash_client_sqlite/src/testing/db.rs
+++ zcash_client_sqlite/src/testing/db.rs
@@ -132,7 +132,7 @@ impl TestDb {
     #[allow(dead_code)]
     #[cfg(feature = "unstable")]
     pub(crate) unsafe fn run_sqlite3(&self, command: &str) {
-        run_sqlite3(self.data_file.path(), command)
+        unsafe { run_sqlite3(self.data_file.path(), command) }
     }
 }
 
