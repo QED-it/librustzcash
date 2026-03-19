2ac6f1fddc4ddbc3b995af8353f40975f69dea77
Constance Beguier <constance@qed-it.com>
2026-03-19 13:39:34 +0100

    Define a BlockHeight for Nu7 TestNetwork

--- components/zcash_protocol/src/consensus.rs
+++ components/zcash_protocol/src/consensus.rs
@@ -526,7 +526,7 @@ impl Parameters for TestNetwork {
             NetworkUpgrade::Nu6 => Some(BlockHeight(2_976_000)),
             NetworkUpgrade::Nu6_1 => Some(BlockHeight(3_536_500)),
             #[cfg(zcash_unstable = "nu7")]
-            NetworkUpgrade::Nu7 => None,
+            NetworkUpgrade::Nu7 => Some(BlockHeight(4_000_000)),
             #[cfg(zcash_unstable = "zfuture")]
             NetworkUpgrade::ZFuture => None,
         }
